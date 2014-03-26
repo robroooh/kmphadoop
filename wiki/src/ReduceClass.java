f9eb03656077c5835b03ba2f8b0c50be507f9e70
robroo.pc@gmail.com <robroo.pc@gmail.com@83188903-fa88-4cd3-6b90-60a3b684288a>
2014-03-26 18:26:14 +0000

    nothing more for tonight // all of them are ready to run i think

--- wiki/src/ReduceClass.java
+++ wiki/src/ReduceClass.java
@@ -9,23 +9,25 @@ import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
 
 public class ReduceClass extends MapReduceBase implements
-		Reducer<Text, PartialString, Text, PartialString> {
+		Reducer<Text, PartialString, Text, String> {
 
 	public void configure(JobConf job) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	public void close() throws IOException {
 		// TODO Auto-generated method stub
-
+		System.out.println("successfully close");
 	}
 
 	public void reduce(Text key, Iterator<PartialString> values,
-			OutputCollector<Text, PartialString> output, Reporter reporter)
+			OutputCollector<Text, String> output, Reporter reporter)
 			throws IOException {
 		// TODO Auto-generated method stub
 		
+		output.collect(key, values.toString());
+		System.out.println(key.toString() + ": " + values.toString());
 	}
 
 }

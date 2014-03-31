2b38fb7c1016e31f7cf6dd621a75a11388cbef0c
robroo.pc@gmail.com <robroo.pc@gmail.com@83188903-fa88-4cd3-6b90-60a3b684288a>
2014-03-31 16:38:57 +0000

    implement as Time&Nut Style

--- wiki/src/StringMatch.java
+++ wiki/src/StringMatch.java
@@ -11,10 +11,13 @@ import org.apache.hadoop.filecache.DistributedCache;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.TextOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 
 
@@ -23,14 +26,29 @@ public class StringMatch {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
-		JobConf jobc = new JobConf();
+		JobConf jobc = new JobConf(StringMatch.class);
+		jobc.setJobName("StringMatching");
 		
+		jobc.setMapOutputKeyClass(Text.class);
+		jobc.setMapOutputValueClass(PartialString.class);
+
+		jobc.setMapperClass(MapperClass.class);
+		jobc.setReducerClass(ReduceClass.class);
+
 		jobc.setInputFormat(PositionInputFormat.class);
-		jobc.setMapperClass((Class<? extends Mapper>) MapperClass.class);
-			
-		FileInputFormat.addInputPath(jobc, new Path(args[0]));
-		FileOutputFormat.setOutputPath(jobc, new Path(args[1]));
-		
-	    Job job = Job.getInstance(jobc);
+		jobc.setOutputFormat(TextOutputFormat.class);
+
+        try {
+			DistributedCache.addCacheFile(new URI("./stringlist.txt"), jobc);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("error from distributedcache in Main");
+		}
+
+        FileInputFormat.setInputPaths(jobc, new Path(args[0]));
+        FileOutputFormat.setOutputPath(jobc, new Path(args[1]));
+
+        JobClient.runJob(jobc);
 	}
 }
